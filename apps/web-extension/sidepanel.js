9b29999b5d31b5d3ede8b28115b1133f2cd811b9
Abhishek K <50524422+overrkill@users.noreply.github.com>
2026-04-13 09:55:20 +0530

    Inline markdown parser for extension (CORS blocked CDN)

--- apps/web-extension/sidepanel.js
+++ apps/web-extension/sidepanel.js
@@ -457,13 +457,27 @@ function highlight(text, query) {
 }
 
 function renderMarkdown(text) {
-  if (typeof marked === 'undefined') {
-    return escHtml(text);
-  }
-  const html = marked.parse(text);
-  if (typeof DOMPurify !== 'undefined') {
-    return DOMPurify.sanitize(html);
-  }
+  let html = escHtml(text);
+  
+  html = html.replace(/^### (.+)$/gm, '<h4>$1</h4>');
+  html = html.replace(/^## (.+)$/gm, '<h3>$1</h3>');
+  html = html.replace(/^# (.+)$/gm, '<h2>$1</h2>');
+  
+  html = html.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>');
+  html = html.replace(/\*(.+?)\*/g, '<em>$1</em>');
+  html = html.replace(/`(.+?)`/g, '<code>$1</code>');
+  
+  html = html.replace(/^- (.+)$/gm, '<li>$1</li>');
+  html = html.replace(/(<li>.*<\/li>\n?)+/g, '<ul>$&</ul>');
+  
+  html = html.replace(/^(\d+)\. (.+)$/gm, '<li>$2</li>');
+  
+  html = html.replace(/^> (.+)$/gm, '<blockquote>$1</blockquote>');
+  
+  html = html.replace(/\n/g, '<br>');
+  html = html.replace(/<br><li>/g, '<li>');
+  html = html.replace(/<\/li><br>/g, '</li>');
+  
   return html;
 }
 
