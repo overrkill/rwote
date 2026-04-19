9ae81d8cd436d41f91cb9fede5b069675d59eaf7
Abhishek K <50524422+overrkill@users.noreply.github.com>
2026-04-19 12:51:51 +0530

    fix Polygon import

--- apps/mobile/src/components/icons.tsx
+++ apps/mobile/src/components/icons.tsx
@@ -1,4 +1,4 @@
-import Svg, { Path, Rect, Line, Circle, Polyline } from 'react-native-svg';
+import Svg, { Path, Rect, Line, Circle, Polyline, Polygon } from 'react-native-svg';
 
 interface IconProps {
   size?: number;
